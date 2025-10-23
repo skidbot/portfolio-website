bbed1fa5f7d8bc4f39079aff2a80946776d602b6
Sam Peller <sam.peller@comcast.net>
2025-10-23 17:06:24 -0400

    Edits

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Highlight active navbar link when scrolling
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
